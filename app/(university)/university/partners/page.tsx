15d3b2914d738c7abfbd03835983d8b97b1be255
anaj00 <jana_bantolino@dlsu.edu.ph>
2026-08-16 16:16:17 +0800

    feat: add query invalidation for Partner MOAs in PartnersContent

--- app/(university)/university/partners/page.tsx
+++ app/(university)/university/partners/page.tsx
@@ -14,6 +14,7 @@ import {
   getUniversityControllerGetAuditLogQueryKey,
   getUniversityControllerGetBlacklistQueryKey,
   getUniversityControllerGetLegacyCompanyQueryKey,
+  getUniversityControllerGetPartnerMoasQueryKey,
   getUniversityControllerListInvitesQueryKey,
   getUniversityControllerListLegacyCompaniesQueryKey,
   getUniversityControllerListPartnersQueryKey,
@@ -795,6 +796,9 @@ function PartnersContent({
     });
     queryClient.invalidateQueries({
       queryKey: getUniversityControllerListRenewalsQueryKey(),
+    });
+    queryClient.invalidateQueries({
+      queryKey: getUniversityControllerGetPartnerMoasQueryKey(detailId),
     });
   };
 
